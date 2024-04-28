a605d4bfe3f64928d8af2584679ea3c6619c42c9
Aris Koxaras <aris@koxaras.com>
2024-04-28 20:57:40 +0300

    Added google analytics

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,6 +51,10 @@ const config: Config = {
         theme: {
           customCss: "./src/css/custom.css",
         },
+        gtag: {
+          trackingID: "G-QWM533F7GX",
+          anonymizeIP: true,
+        },
       } satisfies Preset.Options,
     ],
   ],
